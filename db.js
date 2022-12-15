8057a328aea37f65bc1337e4cedf36ccb5df564b
Tiago Silva <tiagoluizrs@gmail.com>
2022-12-15 19:12:51 -0300

    ...

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 const Sequelize = require('sequelize');
-require("dotenv").config();
+// require("dotenv").config();
 
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
+// const dbName = process.env.DB_NAME;
+// const dbUser = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
+// const dbHost = process.env.DB_HOST;
 
-const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-  //passar os dados para o sequelize
-  dialect: "mysql", //informar o tipo de banco que vamos utilizar
-  host: dbHost, //o host, neste caso estamos com um banco local
-});
+// const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+//   //passar os dados para o sequelize
+//   dialect: "mysql", //informar o tipo de banco que vamos utilizar
+//   host: dbHost, //o host, neste caso estamos com um banco local
+// });
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite'
+})
 
 module.exports = sequelize;
